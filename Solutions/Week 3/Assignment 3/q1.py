ccfb3151bfc8bfaa9b4127be808283de4a025beb
Code and Debug <codeanddebugedu@gmail.com>
2024-02-28 22:22:49 +0530

    week3 assignment 3 solution

---
+++ Solutions/Week 3/Assignment 3/q1.py
@@ -0,0 +1,14 @@
+"""
+Make a list of your own. Print the whole list in reverse using 
+FOR loop and WHILE loop.
+"""
+
+my_list = [43, 65, "Elon", "Ambani", False, 55.43]
+
+# for i in range(0, len(my_list)):
+#     print(my_list[i])
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
