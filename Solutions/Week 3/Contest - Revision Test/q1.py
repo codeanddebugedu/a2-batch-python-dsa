8b6abaf3377a1658d3cbe73bf7876c9577026f4a
Code and Debug <codeanddebugedu@gmail.com>
2024-03-03 13:56:22 +0530

    refactor week 3 contest q1 solution

--- Solutions/Week 3/Contest - Revision Test/q1.py
+++ Solutions/Week 3/Contest - Revision Test/q1.py
@@ -3,6 +3,14 @@ Calculate the cube of all numbers from 1 to a given number.
 """
 
 
+# To just print the cubes
+def print_cube_of_numbers(n: int) -> None:
+    for i in range(1, n + 1):
+        print(i**3, end=" ")
+    print()
+
+
+# To calculate sum of cubes
 def cube_of_numbers(n: int) -> int:
     total = 0
     for i in range(1, n + 1):
@@ -10,5 +18,5 @@ def cube_of_numbers(n: int) -> int:
     return total
 
 
+print_cube_of_numbers(10)
 print(cube_of_numbers(10))
-print(cube_of_numbers(5))
