996a1bd23cb24feece7eb37045249be397ac8b94
Code and Debug <codeanddebugedu@gmail.com>
2024-03-04 08:33:02 +0530

    refactored contest hard q3

--- Solutions/Week 3/Contest - List Hard Questions/q3.py
+++ Solutions/Week 3/Contest - List Hard Questions/q3.py
@@ -1,13 +1,18 @@
 """
 Write a python program to check if the list contains three consecutive common numbers in Python.
 """
+
 from typing import List
 
 
 def isConsecutive(my_list: List[int]) -> None:
+    flag = False
     for i in range(0, len(my_list) - 2):
         if my_list[i] == my_list[i + 1] == my_list[i + 2]:
+            flag = True
             print(my_list[i])
+    if flag == False:
+        print("No")
 
 
 isConsecutive([1, 2, 3, 4, 55, 55, 55, 6, 7, 7, 7])
